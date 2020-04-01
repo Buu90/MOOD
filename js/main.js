19aa5457d6027ebd75e20305623ef7397de22c47
Buu90 <55487031+Buu90@users.noreply.github.com>
2020-04-01 20:45:49 +0300

    Add files via upload
    New

---
+++ js/main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function randomDiap(n, m) {
+  return Math.floor(Math.random() * (m - n + 1)) + n;
+}
+
+function mood(colorsCount) {
+  var colors = [
+    "",
+    "красный",
+    "оранжевый",
+    "жёлтый",
+    "зелёный",
+    "голубой",
+    "синий",
+    "фиолетовый"
+  ];
+
+  console.log("цветов: " + colorsCount);
+  for (var i = 1; i <= colorsCount; i++) {
+    var n = randomDiap(1, colors.length - 1);
+    var colorName = colors.splice(n, 1)[0];
+    console.log(colorName);
+  }
+}
+
+mood(3);
